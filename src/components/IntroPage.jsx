c82cbf8cf098486c9b96c49d22379ae1b2b21c4a
KIMGA000 <wldusrkdud@gmail.com>
2024-08-06 17:53:57 +0900

    Fixed

--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -5,7 +5,7 @@ import './IntroPage.css';
 const IntroPage = () => {
   return (
     <div className="intro">
-      <h1 className="title">Today's Mood</h1>
+      <h1 className="title">Today's<br />Mood</h1>
       <div className="btn-container">
         <Link to="/auth?mode=login">
           <button className="intro-btn">
@@ -17,6 +17,17 @@ const IntroPage = () => {
             SignUp
           </button>
         </Link>
+      </div>
+      <div className="shapes-container">
+        <div className="shape1">
+          <img src="images/Shape1.png" alt="Shape1" />
+        </div>
+        <div className="shape2">
+          <img src="images/Shape2.png" alt="Shape2" />
+        </div>
+        <div className="shape3">
+          <img src="images/Shape3.png" alt="Shape3" />
+        </div>
       </div>
     </div>
   );
